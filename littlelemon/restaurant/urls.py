ba8721f5d9bffa0ae17d36a1700229701c0908c3
ayush-singh17 <ayush.shreya06@gmail.com>
2026-06-23 18:43:42 +0530

    ADD API

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view(), name='menu-items'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
 ]
